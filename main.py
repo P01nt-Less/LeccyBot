a357e23468b27a851d938349c9dbfc08fd5fdefa
Pointless <MindScriptMC@gmail.com>
2018-06-22 21:09:44 +0100

    Initial commit

--- main.py
+++ main.py
@@ -11,57 +11,24 @@ import re # for regular expression operations
 import json # self-explanatory
 import time # self-explanatory
 import asyncio # tasks
-import os # operating system stuff and also heroku lel
-from os import listdir
-from os.path import isfile, join
+import os # operating system stuff and also heroku
+from os import listdir #cogs
+from os.path import isfile, join #cogs
 import datetime#self-explanatory
 import logging #logging
 
 description = 'A multi-purpose bot made with Discord.py in the Async library.'
-prefix = '!'
+prefix = '.'
 
 
 bot = commands.Bot(command_prefix=prefix,description=description)
-
-def pointcheck(ctx):
-    return ctx.message.author.id == '276043503514025984' #checks if @Pointless#1278 is the author of the command
 
 @bot.event
 async def on_ready():
     print('Successful logging in.')
     print('https://discordapp.com/oauth2/authorize?client_id=449301359578316811&scope=bot&permissions=2146958591')
-    await LoadCogs()
-
-@bot.command()
-async def load(extension_name : str):
-    """Loads an extension."""
-    try:
-        bot.load_extension(extension_name)
-    except (AttributeError, ImportError) as e:
-        await bot.say("```py\n{}: {}\n```".format(type(e).__name__, str(e)))
-        return
-    await bot.say("{} loaded.".format(extension_name))
-
-@bot.command()
-async def unload(extension_name : str):
-    """Unloads an extension."""
-    bot.unload_extension(extension_name)
-    await bot.say("{} unloaded.".format(extension_name))
-
-async def LoadCogs():
-    for extension in [f.replace('.py', '') for f in listdir('cogs') if isfile(join('cogs', f))]:
-        try:
-            if not "__init__" in extension:
-                print("Loading {}...".format(extension))
-                bot.load_extension('cogs.' + extension)
-        except Exception as e:
-            print('Failed to load cog {}'.format(extension))
-            traceback.print_exc()
-def Main():
-    logging.basicConfig(level=logging.INFO)
-    if not os.environ.get('TOKEN'):
-        print("No token was found.")
-    bot.run(os.environ.get('TOKEN').strip('"'))
-
-if __name__ == '__main__':
-    Main()
+    bot.session = aiohttp.ClientSession()
+    presence=['!help | May the Leccy be with us.','!help | Python > Javascript','!help | OOF ~ Noob - 2006','!help | Press \'L\' to pay for my electricity bills.','!help | My friend told me how electricity is measured and I was like \'Watt?!?\'','!help | If you plant a light bulb in your garden, does it grow into a power plant?','!help | OOH ~ Old Steve - 2011','!help | https://discord.gg/c8vNvfR is where you should really go.','!help | Heroku doesn\'t suck!','!help | Made by @Pointless#1278','!help | Hello there.','!help | Quite strange, isn\'t it?','!help | BEEP BOOP','!help | Why are you reading these?','!help | Press F to pay respects.','!help | 9 + 10 = 21','!help | The Illuminati is watching.','!help | R.I.P Harambe 2016','!help | Somebody Toucha My Leccy!','!help | Faster than Windows 1.0!','!help | ?','!help | !','!help | Welp...','!help | print(party() + \'!\')','!help | Gotta go fast!']
+    while True:
+        await bot.change_presence(game=discord.Game(name=random.choice(presence)))
+        await asyncio.sleep(5)
